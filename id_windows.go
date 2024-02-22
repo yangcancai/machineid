80a94d538ff2d919a9eb9814ee19d2dbcd89db59
Cam <cam@CamdeMacBook-Pro.local>
2024-02-22 18:29:25 +0800

    Fist check biosuuid

--- id_windows.go
+++ id_windows.go
@@ -1,14 +1,25 @@
+//go:build windows
 // +build windows
 
 package machineid
 
 import (
+	"fmt"
+	"os/exec"
+	"strings"
+
 	"golang.org/x/sys/windows/registry"
 )
 
 // machineID returns the key MachineGuid in registry `HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Cryptography`.
 // If there is an error running the commad an empty string is returned.
 func machineID() (string, error) {
+	if id, e := getSmbiosUUID(); e == nil {
+		return id, nil
+	}
+	if id, e := deviceID(); e == nil {
+		return id, nil
+	}
 	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\Cryptography`, registry.QUERY_VALUE|registry.WOW64_64KEY)
 	if err != nil {
 		return "", err
@@ -20,4 +31,47 @@ func machineID() (string, error) {
 		return "", err
 	}
 	return s, nil
+}
+func deviceID() (string, error) {
+	k, err := registry.OpenKey(registry.LOCAL_MACHINE, `SOFTWARE\Microsoft\SQMClient`, registry.QUERY_VALUE|registry.WOW64_64KEY)
+	if err != nil {
+		return "", err
+	}
+	defer k.Close()
+
+	s, _, err := k.GetStringValue("MachineId")
+	if err != nil {
+		return "", err
+	}
+	return s, nil
+
+}
+func getSmbiosUUID() (uuid string, err error) {
+	defer func() {
+		panc := recover()
+		if panc != nil {
+			err = fmt.Errorf("getSmbiosUUID: %v", panc)
+		}
+	}()
+	cmd := exec.Command("wmic", "csproduct", "get", "UUID")
+	output, err := cmd.Output()
+	flag := false
+	if err == nil {
+		uuid = strings.Split(string(output), "\n")[1]
+		if uuid == "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF" {
+			err = fmt.Errorf("smbios return FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF")
+			flag = false
+		} else {
+			flag = true
+		}
+	}
+	if !flag {
+		cmd = exec.Command("wmic", "bios", "get", "serialnumber")
+		output, err = cmd.Output()
+		if err == nil {
+			uuid = strings.Split(string(output), "\n")[1]
+			flag = true
+		}
+	}
+	return
 }
